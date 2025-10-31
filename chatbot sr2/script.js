de6d4b6174cceb7d36a44fd960453f3959b71037
iamakshat09 <akshat.singh@intellipaat.com>
2025-10-31 15:15:45 +0530

    Add files via upload

---
+++ chatbot sr2/script.js
@@ -0,0 +1,31 @@
+const content=document.querySelector("#content");
+const btn=document.querySelector("#btn");
+
+btn.addEventListener("click",function(){
+    let prompt=document.querySelector("#prompt").value;
+    console.log(prompt)
+    getdata(prompt)
+})
+
+async function getdata(prompt) {
+    const res = await fetch('https://openrouter.ai/api/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            Authorization: 'Bearer <YOURAPIKEY>',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            model: 'tngtech/deepseek-r1t2-chimera:free',
+            messages: [
+                {
+                    role: 'user',
+                    content: prompt,
+                },
+            ],
+        }),
+    });
+    const data = await res.json();
+    console.log(data);
+    console.log(data.choices[0].message.content)
+    content.textContent=data.choices[0].message.content;
+}
